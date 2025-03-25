b5ad0cd57717d3872859494632227fe88f4e77a0
João Miguel Silva Salvalagio <joaosalvalagio@gmail.com>
2025-03-24 22:22:35 -0300

    att

--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@ function somar(){
 
     document.getElementById("resultado").textContent = resultado;
 }
+function subtrair(){
+    let numero1 = parseFloat(document.getElementById("numero1").value);
+    let numero2 = parseFloat(document.getElementById("numero2").value);
+
+    let resultado = numero1 - numero2;
+
+    document.getElementById("resultado").textContent = resultado;
+}
+function dividir(){
+    let numero1 = parseFloat(document.getElementById("numero1").value);
+    let numero2 = parseFloat(document.getElementById("numero2").value);
+
+    let resultado = numero1 / numero2;
+
+    document.getElementById("resultado").textContent = resultado;
+}
+function multiplicar(){
+    let numero1 = parseFloat(document.getElementById("numero1").value);
+    let numero2 = parseFloat(document.getElementById("numero2").value);
+
+    let resultado = numero1 * numero2;
+
+    document.getElementById("resultado").textContent = resultado;
+}
