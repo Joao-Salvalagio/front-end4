4f26a5e0fa4554834ed5945639ad85a09ae3da21
João Miguel Silva Salvalagio <joaosalvalagio@gmail.com>
2025-03-26 20:36:50 -0300

    att

--- js/script.js
+++ js/script.js
@@ -18,6 +18,10 @@ function dividir(){
     let numero1 = parseFloat(document.getElementById("numero1").value);
     let numero2 = parseFloat(document.getElementById("numero2").value);
 
+    if(numero2 == 0){
+        document.getElementById('resultado').innerText = ("Divisão por zero.");
+    }
+
     let resultado = numero1 / numero2;
 
     document.getElementById("resultado").textContent = resultado;
